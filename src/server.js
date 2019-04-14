b7ddb09f99c0b0415e776e7dcdc64baae679b59c
Cristiane Mayara <cris.mayara@gmail.com>
2019-04-14 18:42:08 -0300

    Setting socket.io

--- src/server.js
+++ src/server.js
@@ -4,7 +4,22 @@ const path = require('path');
 
 const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  socket.on('connectRoom', (box) => {
+    socket.join(box);
+  });
+});
+
 mongoose.connect('mongodb://localhost:27017/reactbox-api', { useNewUrlParser: true });
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,4 +27,4 @@ app.use('./files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(require('./routes'));
 
-app.listen(3333);
+server.listen(3333);
