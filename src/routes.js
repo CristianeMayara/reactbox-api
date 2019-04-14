f5b881c94c0f8b3237499761a2dd02bd893f2f55
Cristiane Mayara <cris.mayara@gmail.com>
2019-04-14 16:48:08 -0300

    Adding route to create boxes

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => res.send('ok'));
+const BoxController = require('./controllers/BoxController');
+
+routes.post('/boxes', BoxController.store);
 
 module.exports = routes;
